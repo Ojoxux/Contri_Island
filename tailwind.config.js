915b126f544ea4df4cff1fe55a76a4f9d4d102cf
Jou Okuyama <ojoxux@gmail.com>
2024-12-17 16:31:55 +0900

    refactor: TailwindCSSの設定ファイルを更新
    TailwindCSSの設定ファイルに以下の変更を加えました:
    - daisyuiプラグインを追加しました
    - primary-darkという新しいカラーを追加しました
    - daisyuiの設定を追加しました

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -5,12 +7,19 @@ export default {
     extend: {
       colors: {
         primary: '#A3D977',
+        'primary-dark': '#8BC34A',
         secondary: '#FFF8E1',
       },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [daisyui],
   daisyui: {
     themes: ['light', 'dark'],
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: '',
+    logs: true,
+    themeRoot: '.',
   },
 };
