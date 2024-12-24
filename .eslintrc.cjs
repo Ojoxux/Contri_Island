64e45b70b67514c1f0b6f26d7128d6db3f755d12
Jou Okuyama <ojoxux@gmail.com>
2024-12-25 01:24:41 +0900

    chore: 📦 ESLint に Storybook 推奨設定を追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,13 +4,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
